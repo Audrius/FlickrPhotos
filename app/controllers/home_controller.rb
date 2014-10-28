36adc5da01e9fc2f9a5b789d7addd18c31c346a0
audriusb <audrius@eparaiska.lt>
2014-10-28 11:05:36 +0200

    first init commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,53 @@
+class HomeController < ApplicationController
+    
+  URL_SERVICE = 'https://api.flickr.com/services/rest'
+  METHOD = 'flickr.photos.search'
+  API_KEY = ''
+  MY_API_KEY = ''
+  MY_API_SECRET = ''
+  FORMAT = 'json'
+
+  def welcome
+  end
+
+  def search_photos
+    search_params = params[:search_params]
+    
+    url = "#{URL_SERVICE}/?method=#{METHOD}&api_key=#{MY_API_KEY}&format=json&tags=#{search_params}&nojsoncallback=1&per_page=100&extras=url_z"
+    ##?api_key=#{@api_key}&method=#{method}"
+    url = URI.parse(url)
+    http = Net::HTTP.new url.host, url.port
+    http.use_ssl = true
+    ap url.request_uri
+    response = nil
+    http.start do |res|
+      response = JSON.parse(res.request_get(url.request_uri).body.to_s)
+    end
+
+    puts response
+
+
+    page = response['photos']['page']
+    pages = response['photos']['pages']
+    per_pages = response['photos']['perpage']
+    total    = response['photos']['total']
+
+    puts "Current results page #{page} pages #{pages} per_pages #{per_pages} total #{total}"
+
+    @photos = []
+
+    response['photos']['photo'].each do | photo |
+      ap photo
+      
+      @photos << photo if  true || photo['width_z'] == "640" && photo['height_z'] == "640" 
+    end 
+    
+    #{"id"=>"15027892464", "owner"=>"125421155@N06", "secret"=>"37dedb5339", "server"=>"7560", "farm"=>8, "title"=>"Поняли, да? У нас персональный электрик. 😸💡#кот #кошки #home #cat #catworld #animal #funnycat #kitty #kitten #littlecat #light #tula #tulacity #russia #pet @bez_kota", "ispublic"=>1, "isfriend"=>0, "isfamily"=>0}
+
+    puts search_params  
+    render :welcome
+  end
+
+ 
+
+end
---
+++ app/helpers/home_helper.rb
@@ -0,0 +1,5 @@
+module HomeHelper
+  def get_photo_url(photo)
+    return "https://farm#{photo['farm']}.staticflickr.com/#{photo['server']}/#{photo['id']}_#{photo['secret']}_c.jpg"
+  end
+end
